27e281e8ede86c6d44d4952f58e5d31e47368a46
Ygor Combi <ycombi349@gmail.com>
2023-02-01 23:09:36 -0300

    gulp sass autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
 
 function compilaSass() {
   return gulp
     .src("assets/css/scss/**/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+        browsers: ["last 2 versions"],
+      })
+    )
     .pipe(gulp.dest("assets/css/"));
 }
 
