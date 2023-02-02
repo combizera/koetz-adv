e365e5343fd7e93374e4de9c4d04990e9913dfc5
Ygor Combi <ycombi349@gmail.com>
2023-02-01 22:57:28 -0300

    consegui instalar o gulp sass e fazer funcionar

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
+
+function compilaSass() {
+  return gulp
+    .src("assets/css/scss/**/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("assets/css/"));
+}
+
+gulp.task("sass", compilaSass);
