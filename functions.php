583a898f716fbffeb4efa935df8ad370ed5126af
Ygor Combi <ycombi349@gmail.com>
2023-02-28 10:03:00 -0300

    bug fix functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,22 @@
- <?php
+<?php
+function get_template_name() {
+  if (is_single()) {
+    return get_post_type();
+  }
+  $template_file = get_post_meta( get_THE_ID(), '_wp_page_template', true );
+  if ($template_file == 'default' || !$template_file) {
+    return 'default';
+  }
+  $templates = wp_get_theme()->get_page_templates();
+  return strtolower( $templates[$template_file] );
+}
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
+  if (get_template_name() !== "home new"){
     wp_enqueue_style( 'child-style',
         get_stylesheet_uri(),
         array( 'parenthandle' )
-    );
-} 
+    );}
+}
+
+
